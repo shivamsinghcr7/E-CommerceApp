ed134650f99551b7ec701b813ac1592ba9d328e7
Shivam Singh <shivam.iitk1@gmail.com>
2025-06-16 23:13:51 +0530

    Created categories

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 //Utils
 import connectDB from "./configs/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -19,5 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes );
 
 app.listen(PORT, () => console.log(`Server running on port:${PORT} ❤️`));
--- frontend/src/pages/Admin/UsersList.jsx
+++ frontend/src/pages/Admin/UsersList.jsx
@@ -6,7 +6,9 @@ import {
   useUpdateUserMutation,
 } from "../../redux/api/usersApiSlice";
 import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
-import { FaPowerOff } from "react-icons/fa6";
+import { FaSkullCrossbones } from "react-icons/fa6";
+import { ImCross } from "react-icons/im";
+
 import Loader from "../../components/Loader";
 import { toast } from "react-toastify";
 import Message from "../../components/Message.jsx";
@@ -46,7 +48,7 @@ const UsersList = () => {
   };
 
   const deleteUserHandler = async (userid) => {
-    if (window.confirm("Are you sure?")) {
+    if (window.confirm("Do you really want to delete this user permanently?")) {
       try {
         await deleteUser(userid);
         window.location.reload();
@@ -106,7 +108,7 @@ const UsersList = () => {
                           onClick={() => cancelEditHandler()}
                           className="ml-2 bg-red-500 text-white py-2 px-4 rounded-lg"
                         >
-                          <FaPowerOff />
+                          <ImCross />
                         </button>
                       </div>
                     ) : (
@@ -141,7 +143,7 @@ const UsersList = () => {
                           onClick={() => cancelEditHandler()}
                           className="ml-2 bg-red-500 text-white py-2 px-4 rounded-lg"
                         >
-                          <FaPowerOff />
+                          <ImCross />
                         </button>
                       </div>
                     ) : (
@@ -171,7 +173,7 @@ const UsersList = () => {
                           onClick={() => deleteUserHandler(user._id)}
                           className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded font-semibold"
                         >
-                          <FaTrash />
+                          <FaSkullCrossbones />
                         </button>
                       </div>
                     )}
